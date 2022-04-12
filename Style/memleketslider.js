4a7fa8511dd22ede3e5759afb40162078bde318e
yasemin-yalcinkaya <yaseminsudeyalcinkaya@gmail.com>
2022-04-12 18:11:22 +0300

    Memleketim sayfası tasarlandı
    Memleketim sayfası tasarlandı, ve footer eklendi

---
+++ Style/memleketslider.js
@@ -0,0 +1,23 @@
+var i=0;
+var images=['img/amasya1.jpg','img/amasya6.jpg','img/amasya7.jpg'];
+
+var duration =2000;
+
+function slideImg() 
+{
+	document.slider.src=images[i];
+
+	if (i<images.length-1)
+	 {
+		i++;
+     }
+     else
+     {
+     	i=0;
+     }
+
+   setTimeout("slideImg()",duration);
+
+}
+
+window.onload=slideImg;
